6a925f6656c4de8e825e83c235bec49e752eae20
fedesniderf <federicosch.fs@gmail.com>
2025-07-15 01:48:46 -0300

    Subida limpia

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-      module.exports = {
-        content: [
-          "./src/**/*.{js,jsx,ts,tsx}",
-        ],
-        theme: {
-          extend: {},
-        },
-        plugins: [],
-      }
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      'xs': '375px',
+      ...defaultTheme.screens,
+    },
+  },
+  plugins: [],
+}
